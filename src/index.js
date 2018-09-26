7b63c1f8524f8c621540047607a8584be2d3f7ec
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-09-26 20:53:33 +0300

    Add solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
   // your implementation
-}
+  const args = equation
+    .split(" ")
+    .join("")
+    .replace("*x^2", "/")
+    .replace("*x", "/")
+    .split("/");
+  [a, b, c] = args.map(el => parseInt(el));
+  const solutions = [];
+  const sqrtDescriminant = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+  const x1 = Math.round(-1 * b + sqrtDescriminant) / (2 * a);
+  const x2 = Math.round(-1 * b - sqrtDescriminant) / (2 * a);
+  solutions.push(x1, x2);
+  solutions.sort((a, b) => a - b);
+  return solutions;
+};
